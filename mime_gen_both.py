e196a902a271721c09ead50a0bcfc1769c8d712b
325862401 <friend_21@yahoo.cn>
2012-10-01 17:53:30 +0800

    first commit

---
+++ mime_gen_both.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# MIME generation of embedded multiparts - Chapter 9
+# mime_gen_both.py
+# This program requires Python 2.2.2 or above
+
+from email.MIMEText import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Utils, Encoders
+import mimetypes, sys
+
+def genpart(data, contenttype):
+    maintype, subtype = contenttype.split('/')
+    if maintype == 'text':
+        retval = MIMEText(data, _subtype=subtype)
+    else:
+        retval = MIMEBase(maintype, subtype)
+        retval.set_payload(data)
+        Encoders.encode_base64(retval)
+    return retval
+
+
+def attachment(filename):
+    fd = open(filename, 'rb')
+    mimetype, mimeencoding = mimetypes.guess_type(filename)
+    if mimeencoding or (mimetype is None):
+        mimetype = 'application/octet-stream'
+    retval = genpart(fd.read(), mimetype)
+    retval.add_header('Content-Disposition', 'attachment',
+            filename = filename)
+    fd.close()
+    return retval
+
+messagetext = """Hello,
+
+This is a *great* test message from Chapter 9.  I hope you enjoy it!
+
+-- Anonymous"""
+messagehtml = """Hello 你好,<P>
+This is a <B>great</B> test message from Chapter 9.  I hope you enjoy
+it!<P>
+-- <I>Anonymous</I>"""
+fd = open('goagent.html', 'rb')
+messagehtml = fd.read()
+fd.close()
+msg = MIMEMultipart()
+msg['To'] = 'goagent@goagent.org'
+msg['From'] = 'Goagent <http://code.google.com/p/goagent/>'
+msg['Subject'] = '使用GoAgent看YouTube视频,上BBC学英语'
+msg['Date'] = Utils.formatdate(localtime = 1)
+msg['Message-ID'] = Utils.make_msgid()
+
+body = MIMEMultipart('alternative')
+#body.attach(genpart(messagetext, 'text/plain'))
+body.attach(genpart(messagehtml, 'text/html'))
+msg.attach(body)
+filelist = ['goagenthome.jpg','IE_set.jpg','ie_con_proxy.jpg','proxy_set.jpg']
+for filename in filelist:
+    msg.attach(attachment(filename))
+#print msg.as_string()
---
+++ spread_goagent.py
@@ -0,0 +1,112 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, smtplib, socket
+from mime_gen_both import msg
+import time,random
+
+gmail_server = 'smtp.gmail.com'
+gmail_fromaddr = 'your-id@gmail.com'
+gmail_password = 'xxxxxx'
+
+qq_server = 'smtp.qq.com'
+qq_fromaddr = 'your_qq@qq.com'
+qq_password = 'xxxxxx'
+initaddr = ''
+cnt = 0
+try:
+    last_line = file('success_sendmail.txt', "r").readlines()[-1]
+    if last_line.split('\t')[1].isdigit():
+        cnt =  int(last_line.split('\t')[1]) 
+    else:
+        cnt = 0
+    if len(last_line.split('\t')) >= 3:
+        print "You last sent email successfully at %s" %last_line.split('\t')[2]
+except IOError:
+    print "open success_sendmail.txt occur error"
+    
+def saveintofile():
+    fd = open('success_sendmail.txt','a')    
+    fd.write(str(initaddr)+'\t'+str(cnt)+'\t'+time.asctime()+'\n')
+    fd.close()
+def serverstarttls(s):
+    code = s.ehlo()[0]
+    usesesmtp = 1
+    if not (200 <= code <= 299):
+        usesesmtp = 0
+        code = s.helo()[0]
+        if not (200 <= code <= 299):
+            raise SMTPHeloError(code, resp)
+
+    if usesesmtp and s.has_extn('starttls'):
+        print "Negotiating TLS...."
+        s.starttls()
+        code = s.ehlo()[0]
+        if not (200 <= code <= 299):
+            print "Couldn't EHLO after STARTTLS"
+            sys.exit(5)
+        print "Using TLS connection."
+    else:
+        print "server does not support TLS; using normal connection."
+def smtplogin(server,username, password):
+    try:
+        #You must enable 'smtp' at you QQ mail setting
+        #You must login your gmail by web browser first,otherwise authentication will fail.
+        s = smtplib.SMTP(server)
+        print "%s start TLS" %server
+        serverstarttls(s)
+        try:
+            s.login(username, password)
+        except smtplib.SMTPException, e:
+            print "Authentication failed:", e
+            sys.exit(1)
+    except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
+        e:
+        print " %s connect smtp server error!" %server
+        print e
+        sys.exit(2)
+    else:
+        print "You have connected to %s and login successfully" %server
+        return s
+ 
+try:
+    #You must login your gmail by web browser first,otherwise authentication will fail.
+    gmail_s = smtplogin(gmail_server,gmail_fromaddr, gmail_password)
+    #No need to login QQ mail by web browser.
+    qq_s    = smtplogin(qq_server,qq_fromaddr, qq_password)
+    while 1:
+        initaddr = random.randint(100000000,999999999)
+        toaddrs = str(initaddr)+'@qq.com'
+        time.sleep(1)
+        try:
+            qq_s.sendmail(qq_fromaddr, toaddrs, msg.as_string())
+        except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
+                e:
+            print " *** Your message may not have been sent by QQ server and try gmail again!"
+            if 'Mailbox not found' in repr(e):
+                continue
+            print e
+            try:
+                gmail_s.sendmail(gmail_fromaddr, toaddrs, msg.as_string())
+            except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
+                e:
+                print " *** Your message may not have been sent by gmail server!"
+                print e
+                if 'Daily sending quota exceeded' in repr(e):
+                    exit(0)
+                continue    
+            print "Message successfully sent to one recipient by gmail server"        
+            cnt += 1
+            saveintofile()
+            continue
+
+        print "Message successfully sent to one recipient by QQ server"
+        cnt += 1
+        saveintofile()
+        continue
+                
+except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
+        e:
+    print " *** Your message may not have been sent!"
+    print e
+    sys.exit(1)
