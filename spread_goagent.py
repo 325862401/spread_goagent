d42ffaf5559585e0eba245d536d714a9858be9fc
325862401 <friend_21@yahoo.cn>
2012-11-27 21:14:54 +0800

    appfog check available qq

--- spread_goagent.py
+++ spread_goagent.py
@@ -3,32 +3,15 @@
 
 import sys, smtplib, socket
 from mime_gen_both import msg
-import time,random
+import time,random,logging,threading
 
-gmail_server = 'smtp.gmail.com'
-gmail_fromaddr = 'shaozheng.wu@gmail.com'
-gmail_password = '663696mm'
 
 qq_server = 'smtp.qq.com'
-qq_fromaddr = '325862401@qq.com'
-qq_password = 'qq325862401'
+qq_fromaddr = '1281795027@qq.com'
+qq_password = 'a6857018'
 initaddr = ''
-cnt = 0
-try:
-    last_line = file('success_sendmail.txt', "r").readlines()[-1]
-    if last_line.split('\t')[1].isdigit():
-        cnt =  int(last_line.split('\t')[1]) 
-    else:
-        cnt = 0
-    if len(last_line.split('\t')) >= 3:
-        print "You last sent email successfully at %s" %last_line.split('\t')[2]
-except IOError:
-    print "open success_sendmail.txt occur error"
-    
-def saveintofile():
-    fd = open('success_sendmail.txt','a')    
-    fd.write(str(initaddr)+'\t'+str(cnt)+'\t'+time.asctime()+'\n')
-    fd.close()
+existQQList = []
+qlock = threading.Lock()
 def serverstarttls(s):
     code = s.ehlo()[0]
     usesesmtp = 1
@@ -39,74 +22,124 @@ def serverstarttls(s):
             raise SMTPHeloError(code, resp)
 
     if usesesmtp and s.has_extn('starttls'):
-        print "Negotiating TLS...."
+        logging.info("Negotiating TLS....")
         s.starttls()
         code = s.ehlo()[0]
         if not (200 <= code <= 299):
-            print "Couldn't EHLO after STARTTLS"
-            sys.exit(5)
-        print "Using TLS connection."
+            logging.info("Couldn't EHLO after STARTTLS")
+
+        logging.info("Using TLS connection.")
     else:
-        print "server does not support TLS; using normal connection."
-def smtplogin(server,username, password):
+        logging.info("server does not support TLS; using normal connection.")
+
+
+def smtplogin(server=qq_server,username=qq_fromaddr, password=qq_password):
     try:
         #You must enable 'smtp' at you QQ mail setting
         #You must login your gmail by web browser first,otherwise authentication will fail.
         s = smtplib.SMTP(server)
-        print "%s start TLS" %server
+        logging.info("%s start TLS" ,server)
         serverstarttls(s)
         try:
             s.login(username, password)
         except smtplib.SMTPException, e:
-            print "Authentication failed:", e
-            sys.exit(1)
+            logging.info("Authentication failed:%s", repr(e))
+
     except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
         e:
-        print " %s connect smtp server error!" %server
-        print e
-        sys.exit(2)
-    else:
-        print "You have connected to %s and login successfully" %server
-        return s
- 
-try:
-    #You must login your gmail by web browser first,otherwise authentication will fail.
-    gmail_s = smtplogin(gmail_server,gmail_fromaddr, gmail_password)
-    #No need to login QQ mail by web browser.
-    qq_s    = smtplogin(qq_server,qq_fromaddr, qq_password)
-    while 1:
-        initaddr = random.randint(100000000,999999999)
-        toaddrs = str(initaddr)+'@qq.com'
-        time.sleep(1)
-        try:
-            qq_s.sendmail(qq_fromaddr, toaddrs, msg.as_string())
-        except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
-                e:
-            print e              
-            if 'Mailbox not found' in repr(e):
-                continue
-            print " *** Your message may not have been sent by QQ server and try gmail again!"
-            try:
-                gmail_s.sendmail(gmail_fromaddr, toaddrs, msg.as_string())
-            except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
-                e:
-                print " *** Your message may not have been sent by gmail server!"
-                print e
-                if 'Daily sending quota exceeded' in repr(e):
-                    exit(0)
-                continue    
-            print "Message successfully sent to one recipient by gmail server"        
-            cnt += 1
-            saveintofile()
-            continue
+        loging.info('%s',repr(e))
 
-        print "Message successfully sent to one recipient by QQ server"
-        cnt += 1
-        saveintofile()
-        continue
-                
-except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
-        e:
-    print " *** Your message may not have been sent!"
-    print e
-    sys.exit(1)
+    else:
+        logging.info("You have connected to %s and login successfully",server)
+
+
+        return s
+
+def checkQQbymail(qq_s,initaddr=None):
+    try:
+        #No need to login QQ mail by web browser.
+        #qq_s    = smtplogin(qq_server,qq_fromaddr, qq_password)
+        if not initaddr:
+            initaddr = random.randint(100000000,999999999)
+        #initaddr = 325862401
+        toaddrs = str(initaddr)+'@qq.com'
+
+        qq_s.sendmail(qq_fromaddr, toaddrs, msg.as_string())
+    except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), \
+                e:
+        logging.info('%s',repr(e))
+        if 'Mailbox not found' in repr(e):
+            logging.info('%s does not exist',str(initaddr))
+            return None
+        else:
+            logging.info('%s exist',str(initaddr))
+            return str(initaddr)
+    else:
+        logging.info('successfully send a mail by QQ server')
+        return str(initaddr)
+
+def threadcode():
+    qq_s    = smtplogin(qq_server,qq_fromaddr, qq_password)
+    global existQQList,qlock
+    while 1:
+        time.sleep(1)
+        reval = checkQQbymail(qq_s)
+        if reval:
+            qlock.acquire()
+            try:
+                existQQList.append(reval)
+            finally:
+                qlock.release()
+
+            #logging.info("add a new exist qq num:%s", ','.join(existQQList))
+        while len(existQQList) > 20:
+            time.sleep(1000)
+class runOnceThread:
+
+    mythreadNotexist = True
+
+    def __init__(self):
+        self.thread = runOnceThread.mythreadNotexist and threading.Thread(target=threadcode,name='mythread') or None
+        runOnceThread.mythreadNotexist = False
+
+
+def getexsitQQlist(num=1):
+    global existQQList,qlock
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
+    if not existQQList:
+        t = runOnceThread()
+        if t.thread:
+                t.thread.setDaemon(1)
+                t.thread.start()
+                logging.info('t create thread successfully')
+        else:
+            del t
+    time.sleep(10)
+    if num < 0:
+        return 'Please input a num larger than 0'
+    elif num == 0:
+        return '325862401'
+    elif num > 10:
+        return 'Please input a num smaller than 100，You get too much'
+    QQlistlen = len(existQQList)
+    retQQlist = []
+    qlock.acquire()
+    try:
+        if  QQlistlen< num:
+            retQQlist = existQQList
+            existQQList = []
+        else:
+            retQQlist = existQQList[:num]
+            existQQList = existQQList[num:]
+    finally:
+            qlock.release()
+
+    retQQlist.append(str(QQlistlen))
+    return '|'.join(retQQlist)
+def sendmailto(qq_num):
+    qq_self    = smtplogin(username='325862401@qq.com', password='qq325862401')
+    #toaddrs = qq_num + '@qq.com'
+    return checkQQbymail(qq_self,qq_num)
+
+if __name__ == '__main__':
+    logging.info("got exist QQ list:%s" ,getexsitQQlist())
---
+++ wsgi.py
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, Request, Response
+from spread_goagent import getexsitQQlist,sendmailto
+application = app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return getexsitQQlist()
+
+@app.route('/test')
+def test():
+    return getexsitQQlist(0)
+    #return 'test url'
+
+@app.route('/<int:num>')
+def ret_num(num):
+    return getexsitQQlist(num)
+
+@app.route('/to/<int:qq_num>')
+def send_mailto(qq_num):
+    return sendmailto(qq_num)
+
+
+if __name__ == '__main__':
+    app.run()
